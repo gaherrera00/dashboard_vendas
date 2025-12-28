a17440d4d862e3b4826a912d60f4cab4ace9ddd5
gaherrera00 <gabriel.h.demarchi@gmail.com>
2025-12-27 23:12:25 -0300

    filtragem inicial

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+# 6
 # o streamlit tem que rodar aqui
 # Configurar página
 # Criar sidebar
 # Chamar serviços
 # Exibir resultados
+import streamlit as st
 from src.load_data import juntar
+from src.filters import criar_filtros, aplicar_filtros
+
+st.set_page_config(page_title="Dashboard de Vendas")
 
 df = juntar()
-print(df.head())
+
+filtros = criar_filtros(df)
+df_filtrado = aplicar_filtros(df, filtros)
+
+st.write(df_filtrado)
--- src/filters.py
+++ src/filters.py
@@ -1,6 +1,43 @@
+# 2
 # Filtros mínimos aceitáveis
 # Categoria (multiselect)
 # Produto (dependente da categoria)
 # Estado
 # Forma de pagamento
 # Período (data inicial e final)
+import streamlit as st
+
+
+# def criar_filtros(df):
+#     st.sidebar.title("Filtros")
+#     st.sidebar.write("Selecione as opções")
+
+#     st.sidebar.caption("Categorias")
+#     categorias = st.sidebar.multiselect("Categorias", df["categoria"].unique())
+#     if categorias:
+#         df = df[df["categoria"].isin(categorias)]
+
+import streamlit as st
+
+
+def criar_filtros(df):
+    st.sidebar.header("Filtros")
+
+    categorias = st.sidebar.multiselect("Categoria", sorted(df["categoria"].unique()))
+
+    if "filtros" not in st.session_state:
+        st.session_state["filtros"] = {}
+
+    if st.sidebar.button("Aplicar filtros"):
+        st.session_state["filtros"] = {"categorias": categorias}
+
+    return st.session_state["filtros"]
+
+
+def aplicar_filtros(df, filtros):
+    df_f = df.copy()
+
+    if filtros.get("categorias"):
+        df_f = df_f[df_f["categoria"].isin(filtros["categorias"])]
+
+    return df_f
